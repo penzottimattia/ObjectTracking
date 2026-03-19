624b9f62a16977c95eeb706236721f3e715cedb6
penzottimattia <penzottimattia@gmail.com>
2026-03-19 00:49:28 +0000

    multi objects

--- sam3/sam3/model/geometry_encoders.py
+++ sam3/sam3/model/geometry_encoders.py
@@ -644,8 +644,9 @@ class SequenceGeometryEncoder(nn.Module):
             # boxes are [Num_boxes, bs, 4], normalized in [0, 1]
             # We need to denormalize, and convert to [x, y, x, y]
             boxes_xyxy = box_cxcywh_to_xyxy(boxes)
-            scale = torch.tensor([W, H, W, H], dtype=boxes_xyxy.dtype)
-            scale = scale.pin_memory().to(device=boxes_xyxy.device, non_blocking=True)
+            scale = torch.tensor(
+                [W, H, W, H], dtype=boxes_xyxy.dtype, device=boxes_xyxy.device,
+            )
             scale = scale.view(1, 1, 4)
             boxes_xyxy = boxes_xyxy * scale
             sampled = torchvision.ops.roi_align(
